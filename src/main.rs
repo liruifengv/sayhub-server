c9805729d05f9aa97e67c96c21d3c3ed38f82b47
李瑞丰 <liruifeng1024@gmail.com>
2022-09-08 18:19:11 +0800

    feat: 新增 res 结构体 (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
+mod articles;
+mod response;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use articles::Article;
+use response::ResponseBody;
 
 #[get("/")]
 async fn hello() -> impl Responder {
-  HttpResponse::Ok().body("Hello, world!")
+    HttpResponse::Ok().body("Hello, world!")
 }
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
-  HttpResponse::Ok().body(req_body)
+    HttpResponse::Ok().body(req_body)
+}
+
+#[get("/articles")]
+async fn get_acticles() -> impl Responder {
+    let mut list = Vec::new();
+    list.push(Article {
+        title: String::from("aaa"),
+        link: String::from("aaa"),
+        time: String::from("aaa"),
+    });
+    HttpResponse::Ok().json(ResponseBody::new("success", list))
 }
 
 async fn manual_hello() -> impl Responder {
-  HttpResponse::Ok().body("Hey there!")
+    HttpResponse::Ok().body("Hey there!")
 }
 
 #[actix_web::main]
@@ -19,10 +35,11 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
+            .service(get_acticles)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 3000))?
     .run()
     .await
 }
