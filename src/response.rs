72846645817575c11c2830392d393511d4c758cc
liruifengv <liruifeng1024@gmail.com>
2022-09-14 18:06:42 +0800

    feat: 封装业务错误

--- src/response.rs
+++ src/response.rs
@@ -1,3 +1,5 @@
+use actix_web::{error, HttpResponse};
+use failure::Fail;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -23,6 +25,30 @@ impl Response<()> {
             code: error,
             message: message.to_owned(),
             data: None,
+        }
+    }
+}
+
+#[derive(Fail, Debug)]
+#[allow(dead_code)]
+pub enum BusinessError {
+    #[fail(display = "字段验证失败: {}", field)]
+    ValidationError { field: String },
+    #[fail(display = "服务器错误，请稍后重试.")]
+    InternalError,
+}
+
+impl error::ResponseError for BusinessError {
+    fn error_response(&self) -> HttpResponse {
+        match *self {
+            BusinessError::ValidationError { .. } => {
+                let res = Response::err(10001, &self.to_string());
+                HttpResponse::BadRequest().json(res)
+            }
+            _ => {
+                let res = Response::err(10000, &self.to_string());
+                HttpResponse::InternalServerError().json(res)
+            }
         }
     }
 }
