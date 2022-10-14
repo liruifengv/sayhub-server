595a7eb94e827861498a6be7c2d44d997b47c625
liruifengv <liruifeng1024@gmail.com>
2022-10-14 16:18:35 +0800

    sql

--- database.sql
+++ database.sql
@@ -1,12 +1,13 @@
 drop table if exists todo_list;
 drop table if exists todo_item;
+drop table if exists a;
 
-create table todo_list (
+create table if not exists todo_list (
   id serial primary key,
   title varchar(255) not null
 );
 
-create table todo_item (
+create table if not exists todo_item (
   id serial primary key,
   title varchar(255) not null,
   checked boolean not null default false,
@@ -18,3 +19,7 @@ insert into todo_list (title) values ('list1'), ('list2'), ('list3');
 
 insert into todo_item (title, list_id)
   values ('items1', 1), ('items2', 2), ('items3', 3);
+
+create table if not exists a (id serial primary key, tag int[]);  
+
+insert into a (tag) values (array[6,7,8,9]), (array[6,7,8,9]), (array[6,7,8,9]);
